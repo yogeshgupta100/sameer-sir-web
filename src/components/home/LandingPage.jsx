ce3780c7b012c6ec84ce7205d0deff0b7a6d1a88
Aryav <aryavbhola@gmail.com>
2023-11-09 23:21:08 +0530

    Fix UI issues and add missing footer

--- src/components/home/LandingPage.jsx
+++ src/components/home/LandingPage.jsx
@@ -1,23 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const LandingPage = ({imageurl}) => {
-  return (
-    <div className="landing-page" id='home-main'>
-      <div className="landing-img" style={{backgroundImage:`url(${imageurl})`}}>
-      </div>
-      <div className="centered-text">
-        <h2>
-        User Experience Revolution: Designing Tomorrow's World
-        </h2>
-        <p>
-        Unlocking the Secrets of User-Centered Design: A Comprehensive Journey into UX Excellence"
-        </p>
-        <button type="button" className="btn">
-              Explore now
-        </button>
-      </div>
-    </div>
-  );
+const LandingPage = ({ imageurl }) => {
+	return (
+		<div className="landing-page" id="home-main">
+			<div className="landing-img" style={{ backgroundImage: `url(${imageurl})` }}></div>
+			<div className="centered-text">
+				<h2>User Experience Revolution: Designing Tomorrow's World</h2>
+				<p>Unlocking the Secrets of User-Centered Design: A Comprehensive Journey into UX Excellence"</p>
+				<button type="button" className="btn">
+					Explore now
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default LandingPage;
