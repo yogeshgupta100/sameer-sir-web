d8dff2330a773eebe2931242daf36723f01ecad9
Aryav <aryavbhola@gmail.com>
2023-12-08 00:43:36 +0530

    Added Chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,47 @@
-import './App.css';
-import { BrowserRouter as Router, Navigate , Route, Routes } from 'react-router-dom';
-import Contact from './components/Contact'
-import Home from './components/home/Home';
-import Aboutus from './components/aboutus/Aboutus';
-import Course from './components/courses/Graphic Design/MainGraphic';
-import DownloadSyllabus from './components/forms/DownloadSyllabus';
-import Articlemain from './components/article/Articlemain';
-import Mainresource from './components/resources/Mainresource';
-import SuccessPayment from './components/SuccessPayment';
-import CancelPayment from './components/CancelPayment';
-import PrivacyPolicy from './components/Terms&Conditions/PrivacyPolicy';
-import MainBlog from './components/blogPage/MainBlog';
-import CancellationPolicy from './components/Terms&Conditions/CancellationPolicy';
-import CustomerSupport from './components/Terms&Conditions/CustomerSupport';
-import TermsAndServices from './components/Terms&Conditions/TermsAndServices';
-import Signup from './components/forms/Signup';
+import "./App.css";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import Contact from "./components/Contact";
+import Home from "./components/home/Home";
+import Aboutus from "./components/aboutus/Aboutus";
+import Course from "./components/courses/Graphic Design/MainGraphic";
+import DownloadSyllabus from "./components/forms/DownloadSyllabus";
+import Articlemain from "./components/article/Articlemain";
+import Mainresource from "./components/resources/Mainresource";
+import SuccessPayment from "./components/SuccessPayment";
+import CancelPayment from "./components/CancelPayment";
+import PrivacyPolicy from "./components/Terms&Conditions/PrivacyPolicy";
+import MainBlog from "./components/blogPage/MainBlog";
+import CancellationPolicy from "./components/Terms&Conditions/CancellationPolicy";
+import CustomerSupport from "./components/Terms&Conditions/CustomerSupport";
+import TermsAndServices from "./components/Terms&Conditions/TermsAndServices";
+import Signup from "./components/forms/Signup";
+import Chatbot from "./utility/Chatbot/Chatbot";
 
 function App() {
-  return (
-    <>
-      <Router>
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/contact" element={<Contact/>} />
-        <Route  path="/about" element={<Aboutus />} />
-        <Route path='/forms/downloadSyllabus' element={<DownloadSyllabus/>}/>
-        <Route path='/article' element={<Articlemain/>}/>
-        <Route path='/resource' element={<Mainresource/>}/>
-        <Route path='/courses/:courseId' element={<Course/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/blogs' element={<MainBlog/>}/>
-        <Route path='/success' element={<SuccessPayment/>}/>
-        <Route path='/cancel' element={<CancelPayment/>}/>
-        <Route path='/tnc/customer-support' element={<CustomerSupport />} />
-        <Route path='/tnc/terms-and-services' element={<TermsAndServices />} />
-        <Route path='/tnc/privacy-policy' element={<PrivacyPolicy />} />
-        <Route path='/tnc/cancellation-and-refund-policy' element={<CancellationPolicy />} />
-      </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Chatbot />
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/contact" element={<Contact />} />
+					<Route path="/about" element={<Aboutus />} />
+					<Route path="/forms/downloadSyllabus" element={<DownloadSyllabus />} />
+					<Route path="/article" element={<Articlemain />} />
+					<Route path="/resource" element={<Mainresource />} />
+					<Route path="/courses/:courseId" element={<Course />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/blogs" element={<MainBlog />} />
+					<Route path="/success" element={<SuccessPayment />} />
+					<Route path="/cancel" element={<CancelPayment />} />
+					<Route path="/tnc/customer-support" element={<CustomerSupport />} />
+					<Route path="/tnc/terms-and-services" element={<TermsAndServices />} />
+					<Route path="/tnc/privacy-policy" element={<PrivacyPolicy />} />
+					<Route path="/tnc/cancellation-and-refund-policy" element={<CancellationPolicy />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
-
---
+++ src/utility/Chatbot/Chatbot.jsx
@@ -0,0 +1,57 @@
+import styles from "./chatbot.module.css";
+import { useState, useRef } from "react";
+
+const botMap = new Map([[1, { message: "Hi there!👋 How can I help you?", options: [1, 2, 3, 4, 5] }]]);
+
+const userMap = new Map([
+	[1, { message: "option 1", trigger: 1 }],
+	[2, { message: "option 2", trigger: 1 }],
+	[3, { message: "option 3", trigger: 1 }],
+	[4, { message: "option 4", trigger: 1 }],
+	[5, { message: "option 5", trigger: 1 }],
+]);
+
+export default function Chatbot() {
+	const chatbotRef = useRef();
+
+	const [messages, setMessages] = useState([1]);
+
+	function toggleChatbot() {
+		chatbotRef.current.classList.toggle(styles.active);
+	}
+
+	return (
+		<div ref={chatbotRef}>
+			<button className={styles.btn} onClick={toggleChatbot}>
+				<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+					<path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" />
+				</svg>
+			</button>
+
+			<div className={styles.collapsableContainer}>
+				<div className={styles.chatbox}>
+					<h3 className={styles.heading}>Hi there!👋</h3>
+					<div className={styles.chat}>
+						{messages.map((messageId, index) => (
+							<div className={index % 2 ? styles.user : styles.bot} key={index}>
+								<div>{index % 2 ? userMap.get(messageId).message : botMap.get(messageId).message}</div>
+								{index === messages.length - 1 &&
+									botMap.get(messageId).options.map((optionId, index) => (
+										<button
+											className={styles.option}
+											key={index}
+											onClick={() =>
+												setMessages([...messages, optionId, userMap.get(optionId).trigger])
+											}
+										>
+											{userMap.get(optionId)?.message}
+										</button>
+									))}
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
