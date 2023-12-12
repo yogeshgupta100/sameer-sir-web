ada65c97fa68be51c8f58962e4597a41c8f97388
Aryav <aryavbhola@gmail.com>
2023-12-12 16:52:42 +0530

    Update chat icon and fix HTML formatting

--- src/utility/Chatbot/Chatbot.jsx
+++ src/utility/Chatbot/Chatbot.jsx
@@ -33,9 +33,7 @@ export default function Chatbot() {
 	return (
 		<div ref={chatbotRef}>
 			<button className={styles.btn} onClick={toggleChatbot}>
-				<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
-					<path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" />
-				</svg>
+				<img src="/assets/chat-icon.svg" alt="chat" />
 			</button>
 
 			<div className={styles.collapsableContainer}>
