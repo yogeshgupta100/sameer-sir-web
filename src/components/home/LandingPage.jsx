d4d54e212ea6141a04b065268b8326e78bb9a5cb
yogeshgupta100 <yogeshgupta6524@gmail.com>
2024-01-08 00:41:12 +0530

    home-baner

--- src/components/home/LandingPage.jsx
+++ src/components/home/LandingPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const LandingPage = ({ imageurl , title , subTitle , buttonText , Link}) => {
 	return (
-		<div className="landing-page" id="home-main">
+		<div className="landing-page container" id="home-main">
 			<div className="landing-page-content container">
-			<div className="centered-text">
+			<div className="centered-text container">
 				<h2>{title}</h2>
-				<p>{subTitle}</p>
+				<p>{subTitle}</p> 
 				<a href={Link} style={{textDecoration:"none"}}>
 				<button type="button" className="button btn" style={{
 					borderRadius: "12.32px",
@@ -25,11 +25,11 @@ const LandingPage = ({ imageurl , title , subTitle , buttonText , Link}) => {
 				</button>
 				</a>
 			</div>
-				<span className="landing-img">
+				<span className="landing-img container" style={{marginRight:"3rem"}}>
 					<img src={imageurl} style={{
 						maxWidth:"40vw",
 						aspectRatio:"1/0.99",
-						marginLeft:"0.5rem"
+						// marginLeft:"0.5rem"
 					}}/>
 				</span>
 			</div>
