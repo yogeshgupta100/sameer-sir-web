388e892c9c447ff667acfbf788aeb2b8034acee5
yogeshgupta100 <yogeshgupta6524@gmail.com>
2024-01-15 21:45:16 +0530

    banner-text

--- src/components/home/LandingPage.jsx
+++ src/components/home/LandingPage.jsx
@@ -9,29 +9,29 @@ const LandingPage = ({ imageurl , title , subTitle , buttonText , Link}) => {
 				<h2>{title}</h2>
 				<p>{subTitle}</p> 
 				<div style={{display:"flex" , width:"70%" , justifyContent:"flex-start" , gap:"1rem"}}>
-				<a href={Link} style={{textDecoration:"none"}}>
+				{/* <a href={Link} style={{textDecoration:"none"}}> */}
 				<button type="button" className="button btn" style={{
 					borderRadius: "12.32px",
 					border: "0.104vw solid #FFF",
 					fontSize:"clamp(0.44rem , 2vw , 1.25rem)",
 					fontWeight:"500",
-					minWidth: "35%",
+					minWidth: "50%",
 					aspectRatio: "10/2.8",
 					padding:"0 1.6vw",
 					margin:"2.22vw 0",
-					whiteSpace:"nowrap"
+					whiteSpace:"nowrap",
 				}}>
 				<span>
               {buttonText}
         </span>
 				</button>
-				</a>
+				{/* </a> */}
 				<button type="button" className="purple-button" id="button" style={{
 					borderRadius: "12.32px",
 					border: "0.104vw solid #FFF",
 					fontSize:"clamp(0.44rem , 2vw , 1.25rem)",
 					fontWeight:"500",
-					minWidth: "35%",
+					minWidth: "50%",
 					aspectRatio: "11/2.7",
 					padding:"0 1.6vw",
 					margin:"2.22vw 0",
